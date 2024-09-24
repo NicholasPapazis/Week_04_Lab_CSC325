311a7a764e26845fd70696f502b351689aa08a73
Hashim Sayyed <hashimsayyed76@gmail.com>
2024-09-23 23:18:51 -0400

    finish staffEmployees

--- src/main/java/com/mycompany/agency/StaffEmployee.java
+++ src/main/java/com/mycompany/agency/StaffEmployee.java
@@ -1,40 +1,38 @@
 package com.mycompany.agency;
 
+// TODO 1: Make StaffEmployee a child of StaffMember
 
-    // TODO 1: Make StaffEmployee a child of StaffMember
-
-public class StaffEmployee 
-{
+public class StaffEmployee extends StaffMember {
     protected String socialSecurityNumber;
     protected double payRate;
 
-    //-----------------------------------------------------------------
-    //  TODO 2: Sets up this staff employee with the specified
-    //  information.
-    //-----------------------------------------------------------------
+    // -----------------------------------------------------------------
+    // TODO 2: Sets up this staff employee with the specified
+    // information.
+    // -----------------------------------------------------------------
     public StaffEmployee(String eName, String eAddress, String ePhone,
-            String socSecNumber, double rate)
-    {
+            String socSecNumber, double rate) {
         super(eName, eAddress, ePhone);
 
         socialSecurityNumber = socSecNumber;
         payRate = rate;
     }
 
-    //-----------------------------------------------------------------
-    // TODO 3: complete the implementation of toString to return information 
-    //          about a staff employee as a string.
-    //-----------------------------------------------------------------
-    public String toString()
-    {
-    
+    // -----------------------------------------------------------------
+    // TODO 3: complete the implementation of toString to return information
+    // about a staff employee as a string.
+    // -----------------------------------------------------------------
+    public String toString() {
+        return "Name: " + super.name + "\n" +
+                "Address: " + super.address + "\n" +
+                "Phone: " + super.phone + "\n" +
+                "Social Security Number: " + this.socialSecurityNumber;
     }
 
-    //-----------------------------------------------------------------
-    // TODO 4:  Returns the pay rate for this staff employee.
-    //-----------------------------------------------------------------
-    public double pay()
-    {
-       
+    // -----------------------------------------------------------------
+    // TODO 4: Returns the pay rate for this staff employee.
+    // -----------------------------------------------------------------
+    public double pay() {
+        return payRate;
     }
 }
